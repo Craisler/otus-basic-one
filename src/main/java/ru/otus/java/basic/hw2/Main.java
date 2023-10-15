7149d63a75a287351fe697c5516caf11b58c3f53
Jurkin Sergey <jurkin@auction-house.ru>
2023-10-15 12:34:16 +0300

    1

--- src/main/java/ru/otus/java/basic/hw2/Main.java
+++ src/main/java/ru/otus/java/basic/hw2/Main.java
@@ -1,4 +1,12 @@
 package ru.otus.java.basic.hw2;
 
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <=7; i++)
+            System.out.println("i");
+    }
+
+    public static void metod2(String[] args) {
+
+    }
 }
