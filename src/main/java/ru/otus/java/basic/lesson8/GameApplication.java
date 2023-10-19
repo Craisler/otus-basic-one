fb4e93217ede5d38acc8e47e1397cfe55a158d20
Jurkin Sergey <jurkin@auction-house.ru>
2023-10-20 02:59:17 +0300

    init

--- src/main/java/ru/otus/java/basic/lesson8/GameApplication.java
+++ src/main/java/ru/otus/java/basic/lesson8/GameApplication.java
@@ -11,15 +11,25 @@ public class GameApplication {
 
     static Scanner scanner = new Scanner(System.in);
     static char[][] map;
+    static char humanSymbol, aiSymbol;
 
     public static void main(String[] args) {
+        System.out.println("Настройка игры. Выберите чем ходить: 1 - X, 2 - O");
+        int answer = scanner.nextInt();
+        if (answer == 2) {
+            humanSymbol = O_SYMBOL;
+            aiSymbol = X_SYMBOL;
+        } else {
+            humanSymbol = X_SYMBOL;
+            aiSymbol = O_SYMBOL;
+        }
         initMap();
         printMap();
         System.out.println("Игра начата");
         while (true) {
             playerTurn();
             printMap();
-            if (checkWin(X_SYMBOL)) {
+            if (checkWin(humanSymbol)) {
                 System.out.println("Вы победили!");
                 break;
             }
@@ -29,7 +39,7 @@ public class GameApplication {
             }
             aiTurn();
             printMap();
-            if (checkWin(O_SYMBOL)) {
+            if (checkWin(aiSymbol)) {
                 System.out.println("Победил компьютер");
                 break;
             }
@@ -41,20 +51,21 @@ public class GameApplication {
         System.out.println("Игра завершена");
     }
 
-    public static boolean checkWin(char symbol){
-        if (map[0][0] == symbol && map [0][1] == symbol && map [0][2] == symbol) return true;
-        if (map[1][0] == symbol && map [1][1] == symbol && map [1][2] == symbol) return true;
-        if (map[2][0] == symbol && map [2][1] == symbol && map [2][2] == symbol) return true;
+    public static boolean checkWin(char symbol) {
+        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) return true;
+        if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) return true;
+        if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol) return true;
 
-        if (map[0][0] == symbol && map [1][0] == symbol && map [2][0] == symbol) return true;
-        if (map[0][1] == symbol && map [1][1] == symbol && map [2][1] == symbol) return true;
-        if (map[0][2] == symbol && map [1][2] == symbol && map [2][2] == symbol) return true;
+        if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol) return true;
+        if (map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol) return true;
+        if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) return true;
 
-        if (map[0][0] == symbol && map [1][1] == symbol && map [2][2] == symbol) return true;
-        if (map[0][2] == symbol && map [1][1] == symbol && map [2][0] == symbol) return true;
+        if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
+        if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol) return true;
 
         return false;
     }
+
     public static boolean isMapFull() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -65,14 +76,46 @@ public class GameApplication {
         }
         return true;
     }
+
+    private static int[] aiFindBestCell(char targetSymbol) {
+        int[] out = new int[]{-1, -1};
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == EMPTY_SYMBOL) {
+                    map[i][j] = targetSymbol;
+                    if (checkWin(targetSymbol)) {
+                        out = new int[]{j, i};
+                    }
+                    map[i][j] = EMPTY_SYMBOL;
+                    if (out[0] > -1) {
+                        return out;
+                    }
+                }
+            }
+        }
+        return out;
+    }
+
     public static void aiTurn() {
         int x, y;
-        do {
-            x = (int) (Math.random() * SIZE);
-            y = (int) (Math.random() * SIZE);
-        } while (!isCellValid(x, y));
+        int[] bestCells;
+        bestCells= aiFindBestCell(aiSymbol);
+        x = bestCells[0];
+        y = bestCells[1];
+        if (x == -1 && y == -1) {
+            int[] cellToBlock =aiFindBestCell(humanSymbol);
+            x = cellToBlock[0];
+            y = cellToBlock[1];
+        }
+        if (x == -1 && y == -1) {
+            do {
+                x = (int) (Math.random() * SIZE);
+                y = (int) (Math.random() * SIZE);
+            } while (!isCellValid(x, y));
+        }
+
         System.out.println("Ход компьютера: " + (x + 1) + "" + (y + 1));
-        map[y][x] = O_SYMBOL;
+        map[y][x] = aiSymbol;
     }
 
     public static void playerTurn() {
@@ -83,7 +126,7 @@ public class GameApplication {
             y = scanner.nextInt() - 1;
         } while (!isCellValid(x, y));
 
-        map[y][x] = X_SYMBOL;
+        map[y][x] = humanSymbol;
     }
 
     public static boolean isCellValid(int x, int y) {
