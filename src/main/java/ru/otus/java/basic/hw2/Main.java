3ac7b424ab0a6e2060ca201bcf97544cd2097425
Jurkin Sergey <jurkin@auction-house.ru>
2023-10-19 20:04:44 +0300

    Правки по замечания с ревью

--- src/main/java/ru/otus/java/basic/hw2/Main.java
+++ src/main/java/ru/otus/java/basic/hw2/Main.java
@@ -5,78 +5,99 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
-        metod1(5, "Строка");
+        printStringSize(5, "Строка");
+        System.out.println(" ");
+        
+        sumValueMoreFive(2, 4, 5, 6, 6, 3, 4);
         System.out.println(" ");
 
-        metod2(2,4,5,6,6,3,4);
+        FillNum(5, 2, 2, 3, 4, 5, 6);
         System.out.println(" ");
 
-        metod3(5, 2, 2, 3, 4, 5, 6);
+        increaseElementNum(10, 1, 3, 44, 456, 456);
         System.out.println(" ");
 
-        metod4(10,1, 3, 44, 456, 456);
-        System.out.println(" ");
-
-        metod5(1, 3, 2, 1);
+        arraySumHalfMore(1, 3, 2, 1);
         System.out.println(" ");
     }
 
-// -Реализуйте метод,
-
-    public static void metod1(int n, String stroka) {
-        // в качестве аргументов целое число и строку, и печатающий в консоль строку указанное количество раз
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Введите число повторения вывода текста");
-//        int n = scanner.nextInt();
-//        Scanner scanner2 = new Scanner(System.in);
-//        System.out.println("Введите выводимое слово");
-//        String s = scanner.nextLine();
+    /**
+     * Метод принимающий в качестве аргументов целое число и строку, и печатающий в консоль строку указанное количество раз
+     *
+     * @param n    количество распечатываемых строк
+     * @param text распечатываемая строка
+     */
+    public static void printStringSize(int n, String text) {
+        System.out.println("Задача 1: Метод принимающий в качестве аргументов целое число и строку, и печатающий в консоль строку указанное количество раз");
         for (int i = 1; i <= n; i++)
-            System.out.println(stroka + " " + i);
+            System.out.println(text + " " + i);
     }
 
-    public static void metod2(int... arr) {
-        // в качестве аргумента целочисленный массив, суммирующий все элементы, значение которых больше 5, и печатающий полученную сумму в консоль.
+    /**
+     * Метод принимающий в качестве аргумента целочисленный массив, суммирующий все элементы, значение которых больше 5, и печатающий полученную сумму в консоль.
+     *
+     * @param arr входной массив
+     */
+    public static void sumValueMoreFive(int... arr) {
+        System.out.println("Задача 2: Метод принимающий в качестве аргумента целочисленный массив, суммирующий все элементы, значение которых больше 5, и печатающий " +
+                "полученную сумму в консоль.");
         int sum = 0;
-        for (int arrNumber = 0; arrNumber < arr.length; arrNumber++) {
-            if (arr[arrNumber] > 5) {
-                sum += arr[arrNumber];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 5) {
+                sum += arr[i];
             }
         }
-
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Массив:" + Arrays.toString(arr));
         System.out.println("Сумма чисел в массиве больше 5 = " + sum);
     }
 
-    public static void metod3(int numberArr, int... arr) {
-        // в качестве аргументов целое число и ссылку на целочисленный массив, метод должен заполниться каждую ячейку массива указанным числом.
-        for (int numArrays = 0; numArrays < arr.length; numArrays++) {
-            arr[numArrays] = numberArr;
-
+    /**
+     * Метод принимающий в качестве аргументов целое число и ссылку на целочисленный массив, метод должен заполниться каждую ячейку массива указанным числом.
+     *
+     * @param num целое число
+     * @param arr входной массив
+     */
+    public static void FillNum(int num, int... arr) {
+        System.out.println("Задача 3: Метод принимающий в качестве аргументов целое число и ссылку на целочисленный массив, метод должен заполниться каждую ячейку " +
+                "массива указанным числом.");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = num;
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Массив:" + Arrays.toString(arr));
     }
 
-    public static void metod4(int numberArr, int... arr) {
-//    в качестве аргументов целое число и ссылку на целочисленный массив, увеличивающий каждый элемент которого на указанное число
-        for (int arrNumber = 0; arrNumber < arr.length; arrNumber++) {
-            arr[arrNumber] = arr[arrNumber] + numberArr;
+    /**
+     * Метод принимающий в качестве аргументов целое число и ссылку на целочисленный массив, увеличивающий каждый элемент которого на указанное число
+     *
+     * @param num
+     * @param arr
+     */
+    public static void increaseElementNum(int num, int... arr) {
+        System.out.println("Задача 4: Метод принимающий в качестве аргументов целое число и ссылку на целочисленный массив, увеличивающий каждый " +
+                "элемент которого на указанное число");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] + num;
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Массив: " + Arrays.toString(arr));
     }
 
-    //    принимающий в качестве аргумента целочисленный массив и печатающий в консоль сумму элементов какой из половин массива больше.
-    public static void metod5(int... arr) {
+    /**
+     * Метод принимающий в качестве аргумента целочисленный массив и печатающий в консоль сумму элементов какой из половин массива больше.
+     *
+     * @param arr входной массив
+     */
+    public static void arraySumHalfMore(int... arr) {
+        System.out.println("Задача 5: Метод принимающий в качестве аргумента целочисленный массив и печатающий в консоль сумму элементов какой " +
+                "из половин массива больше.");
         int sum1 = 0;
         int sum2 = 0;
-        for (int arrNumber = 0; arrNumber < arr.length / 2; arrNumber++) {
-            sum1 += arr[arrNumber];
+        for (int i = 0; i < arr.length / 2; i++) {
+            sum1 += arr[i];
         }
-        for (int arrNumber = arr.length / 2; arrNumber < arr.length; arrNumber++) {
-            sum2 += arr[arrNumber];
+        for (int i = arr.length / 2; i < arr.length; i++) {
+            sum2 += arr[i];
         }
-
+        System.out.println("Массив: " + Arrays.toString(arr));
         if (sum1 == sum2) {
             System.out.println("Первая и вторая половина массива равны");
         } else if (sum1 > sum2) {
@@ -85,5 +106,4 @@ public class Main {
             System.out.println("Вторая половина массива больше");
         }
     }
-
 }
