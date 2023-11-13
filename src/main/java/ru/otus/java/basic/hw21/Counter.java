fe24f68281a6232daf64a3ebea633e8eb65dd479
Журкин Сергей <jurkin@auction-house.ru>
2023-11-13 18:47:12 +0300

    Домашнее задание 21

---
+++ src/main/java/ru/otus/java/basic/hw21/Counter.java
@@ -0,0 +1,13 @@
+package ru.otus.java.basic.hw21;
+
+public class Counter {
+    private int value;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
